b2c95f66f96433e4d02aeb11fc7a0e09239e208e
AunCly <aurelien.clugery@gmail.com>
2018-05-17 12:02:40 +0200

    changements mineurs

--- hotfix.sh
+++ hotfix.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-# définition des variables
-#change=false
-
 # on se place dans le bon repo
 cd && cd sites/site_stimbiz
 
@@ -13,7 +10,6 @@ tag=`git describe --abbrev=0 --tags`
 message=`pwd`
 echo "Vous êtes dans le dossier $message version $tag"
 
-
 # On explode la chain de la version pour recup le numéro de tag courrant
 IFS='.' read -r -a array <<< "$tag"
 hotfix="${array[2]}"
@@ -21,27 +17,26 @@ hotfix="${array[2]}"
 # Creation du nouveau numéro de tag
 let new_tag="$(($hotfix + 1))"
 
-echo "new tag $new_tag"
-
 # on check si il y a des changement en cours
 if git diff-index --quiet HEAD --; then
-	echo "no uncommited files"
+	echo "Pas de changements en cours"
 else
 	read -p 'Changements non commité stash les changements ? Y/N : ' response
 	# Si oui on propose de les stash pour créer le hotfix
 	if [ $response = "Y" ] ; then
 		change="true"
 		echo "On stash les chagements"
-    	#git stash
+    	git stash
     #si non on force le fermeture du script	
     else
-    	echo "On ne stash pas les chagements et on quitte le script"
+    	echo "Les changements doivent être stash avant la création du hotfix"
     	exit 1
 	fi
 fi
 
 # creation du hotfix
-git flow hotfix start "${array[0]}.${array[1]}.$tag"
+echo "Création du hotfix : ${array[0]}.${array[1]}.$new_tag"
+git flow hotfix start "${array[0]}.${array[1]}.$new_tag"
 
 # on recupere les changement stash
 if [ change = "true" ]; then
